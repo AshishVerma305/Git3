539082ec3971cca73f688a8e56fc809ef5412ce1
ashishverma <ashishvermahell4@gmail.com>
2021-02-16 18:39:32 +0530

    COMMIT MESSAGE

--- src/demo2.java
+++ src/demo2.java
@@ -1,2 +1,9 @@
-public class demo2 {
+import java.sql.SQLOutput;
+
+public class demo2
+{
+    public static void main(String[] args) {
+        System.out.println("hello world:");
+        System.out.println("Hello world 2");
+    }
 }
